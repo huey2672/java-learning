5161328f35ccafbebdbe9ea7ea526fde5c77abc9
huey <huey2672@163.com>
2020-12-22 09:37:50 +0800

    refactor

--- open-source/cxf/integration-with-springboot/src/main/java/com/huey/learning/java/cxf/springboot/config/CxfConfiguration.java
+++ open-source/cxf/integration-with-springboot/src/main/java/com/huey/learning/java/cxf/springboot/config/CxfConfiguration.java
@@ -1,7 +1,6 @@
 package com.huey.learning.java.cxf.springboot.config;
 
 import com.huey.learning.java.cxf.springboot.sei.HelloService;
-import com.huey.learning.java.cxf.springboot.sei.impl.HelloServiceImpl;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.apache.cxf.transport.servlet.CXFServlet;
@@ -20,17 +19,16 @@ import javax.xml.ws.Endpoint;
 public class CxfConfiguration {
 
     @Bean("CXFServlet")
-    public ServletRegistrationBean dispatcherServlet() {
+    public ServletRegistrationBean registerCxfServlet() {
         // 注册 CXFServlet，拦截 /ws/ 开头的请求，不设置则默认拦截 /services/*
-        return new ServletRegistrationBean(new CXFServlet(), "/ws/*");
+        return new ServletRegistrationBean<>(new CXFServlet(), "/ws/*");
     }
 
     @Bean("helloEndpoint")
-    public Endpoint endpoint(SpringBus cxf, HelloService helloService) {
-        Endpoint endpoint = new EndpointImpl(cxf, helloService);
+    public Endpoint endpoint(SpringBus springBus, HelloService helloService) {
+        Endpoint endpoint = new EndpointImpl(springBus, helloService);
         endpoint.publish("/hello");
         return endpoint;
     }
-
 
 }
