0a522fe199ef65125aca549b2f1f4b6228660fce
huey2672 <huey2672@163.com>
2021-12-27 16:57:00 +0800

    thread group

---
+++ concurrent/java-concurrent-learning/src/main/java/com/huey/learning/java/concurrent/thread/group/Result.java
@@ -0,0 +1,18 @@
+package com.huey.learning.java.concurrent.thread.group;
+
+/**
+ * @author huey
+ */
+public class Result {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
