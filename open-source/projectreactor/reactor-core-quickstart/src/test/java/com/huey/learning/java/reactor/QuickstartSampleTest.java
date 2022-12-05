2df411ceb154051a7feb336dede65f5d80ceebf1
huey2672 <huey2672@163.com>
2022-12-05 16:35:24 +0800

    reactor core quick start

--- open-source/projectreactor/reactor-core-quickstart/src/test/java/com/huey/learning/java/reactor/QuickstartSampleTest.java
+++ open-source/projectreactor/reactor-core-quickstart/src/test/java/com/huey/learning/java/reactor/QuickstartSampleTest.java
@@ -1,16 +1,24 @@
 package com.huey.learning.java.reactor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
+@Slf4j
 public class QuickstartSampleTest {
 
     @Test
@@ -28,9 +36,11 @@ public class QuickstartSampleTest {
 
     @Test
     public void testMono() {
+
         Mono.just("foo")
                 .log()
                 .subscribe(System.out::println);
+
     }
 
 }
