9eed0679cdcfcd350e591f3141dadb72b4cfabe0
zheng.zhisen <zheng.zhisen@iwhalecloud.com>
2023-05-18 00:21:39 +0800

    java 9 new feature

--- java9+/java9-new-features/src/com/huey/learning/java/feature/java9/TryWithResourcesExtensionSample.java
+++ java9+/java9-new-features/src/com/huey/learning/java/feature/java9/TryWithResourcesExtensionSample.java
@@ -1,9 +1,5 @@
 package com.huey.learning.java.feature.java9;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class TryWithResourcesExtensionSample {
