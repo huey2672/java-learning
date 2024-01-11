af55ddc3e80ce49bc5bf3a0e3fae4c95b564c3da
huey2672 <huey2672@163.com>
2024-01-12 00:08:33 +0800

    cxf

--- open-source/cxf/integration-with-springboot/src/main/java/com/huey/learning/java/cxf/springboot/sei/HelloService.java
+++ open-source/cxf/integration-with-springboot/src/main/java/com/huey/learning/java/cxf/springboot/sei/HelloService.java
@@ -1,11 +1,13 @@
 package com.huey.learning.java.cxf.springboot.sei;
 
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 @WebService(targetNamespace = "http://www.huey.com")
 public interface HelloService {
 
+    @WebResult(name = "response")
     String sayHello(@WebParam(name = "name") String name);
 
 }
